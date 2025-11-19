4af504e02d9179b51c14c6b1d6107aa387b89172
Arnav Kudiyal <reacharnavkudiyal@gmail.com>
2025-11-19 17:17:09 +0530

    Add min and max age finder program
    Implement a program to find the minimum and maximum ages from user input. and also print the index value in which those minimum and maximum are stored.

---
+++ Array/min&max finder.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main()
+{
+    int index_largest = 0;
+    int index_smallest = 0;
+    int age[10];
+    int size = sizeof(age) / sizeof(int);
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Enter the age of " << i+1 << " person" << endl;
+        cin >> age[i];
+    }
+    int start = age[0];
+    int start2 = age[0];
+
+
+    //For largest age
+    for(int i = 0; i < size; i++)
+    {
+        if(age[i] >= start)
+        {
+            index_largest = i;
+            start = age[i];
+        }
+    }
+    
+    cout << "The largest age =" << start << endl;
+    cout << "The index of largest age = " << index_largest << endl;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(age[i] <= start2)
+        {
+            index_smallest = i;
+            start2 = age[i];
+        }
+    }
+
+    cout << "The smallest age =" << start << endl;
+    cout << "The index of smallest age = " << index_smallest << endl;
+
+
+    return 0;
+}
